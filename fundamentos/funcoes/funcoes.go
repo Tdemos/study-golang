66332f4f91f2c101ab42b091c14b8190a064f0dc
Cassio Glay <cassioglay@MacBook-Pro-de-Cassio.local>
2021-05-17 15:34:03 -0300

    Basico - Funcoes

---
+++ fundamentos/funcoes/funcoes.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func soma(a int, b int) int {
+	return a + b
+}
+
+func imprimir(resultado int) {
+
+	fmt.Println(resultado)
+}
---
+++ fundamentos/funcoes/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	resultado := soma(4, 9)
+	imprimir(resultado)
+}
